af13cee5b5e9d5acd9a6116113f5e65d80551bf4
Wilhelm Behncke <behncke@sitegeist.de>
2015-10-29 19:18:25 +0100

    [TASK] Add some unit test boilerplate

---
+++ Tests/Unit/Domain/Factory/FixtureFactoryTest.php
@@ -0,0 +1,9 @@
+<?php
+namespace PackageFactory\Fixtures\Tests\Unit\Domain\Factory;
+
+/**
+ * Testcase for the fixture factory
+ */
+class FixtureFactoryTest extends \TYPO3\Flow\Tests\UnitTestCase {
+
+}
---
+++ Tests/Unit/Domain/Service/FixtureServiceTest.php
@@ -0,0 +1,9 @@
+<?php
+namespace PackageFactory\Fixtures\Tests\Unit\Domain\Service;
+
+/**
+ * Testcase for the fixture service
+ */
+class FixtureServiceTest extends \TYPO3\Flow\Tests\UnitTestCase {
+
+}
